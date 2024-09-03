c86bb487f043efc0eb574d01c16ba87a135dc5a1
Vebjørn Solberg Hamnvik <vebjorsh@hiof.no>
2024-09-03 15:06:34 +0200

    Ferdig oppgave 3

--- src/oppgave3.js
+++ src/oppgave3.js
@@ -1,69 +1,69 @@
 import "./styles.css";
 
-// TODO: Sett startverdien for de ulike tellerene
-let wordCounter;
-let positionCounter;
-let wrongCounter;
+let wordCounter = 0;
+let positionCounter = 0;
+let wrongCounter = 0;
 
-// TODO: Lag en liste med ulike ord
-const words = "";
+const words = ["Pannekaker", "Carbonara", "Lasagne", "Pizza", "Hamburger", "Lapskaus", "Fiskegrateng"];
 
-// TODO: Hent ut HTML #id og knappen
-const word = "";
-const wrongs = "";
-const letter = "";
-const button = "";
+const word = document.getElementById("word")
+const wrongs = document.getElementById("wrongs")
+const letter = document.getElementById("letter")
+const button = document.getElementById("button")
 
 const setWord = () => {
-  // TODO: Skriv ut ordet som brukeren skal skrive eller en medling om at det ikke er flere ord igjen
-  word.innerHTML = "";
+  if (wordCounter < words.length) {
+    word.innerHTML = words[wordCounter]
+  } else {
+    word.innerHTML = "Ingen flere ord"
+  }
+  
 };
 
 const changeWord = () => {
-  // TODO: Nullstill posisjonstelleren
-  // TODO: Oppdater telleren vi bruker for å velge ut ord
+  positionCounter = 0
+  wordCounter++
   setWord();
 };
 
-// TODO: Sjekk vi har skrevet riktig bokstav. Må ta hensyn til plassen i ordet vi skal skrive
 const checkPosition = (word, position, letter) => {
-  return "";
+  return word[position].toLowerCase() === letter.toLowerCase();
 };
 
-// TODO: Sjekk om posisjonen vi er på er lik lengden på ordet vi skal skrive. Det betyr at vi er ferdig med ordet og kan bytte ord
 const wordIsCorrect = (word, position) => {
-  return "";
+  return position === word.length;
 };
 
 const handleKeyUp = ({ key }) => {
-  // TODO: Hent ut ordet vi skal skrive
-  const word = "";
-  // TODO: Bruk checkPosition() til å sjekke om vi har skrevet rett bokstav
-  if ("") {
-    // TODO: Øk posisjonstelleren
-    // TODO: Bruk wordIsCorrect() til å sjekke om vi er ferdig med ordet
-    if ("") {
-      // TODO: Trigg funksjonen som bytter ord
+
+  const currentWord = words[wordCounter] || "";
+
+  if (checkPosition(currentWord, positionCounter, key)) {
+    positionCounter++
+    if (wordIsCorrect(currentWord, positionCounter)){
+      changeWord();
     }
   } else {
-    // TODO: Oppdater telleren for "feil"
+    wrongCounter++
   }
   updateUI(key);
 };
 
 const updateUI = (key) => {
-  // TODO: Sjekk om det er flere ord igjen
-  if ("") {
-    // TODO: Oppdater HTML for
-    // - antall feil
-    // - bokstaven vi har skrevet
-    // - stylingen på ordet basert på hvilken posisjon vi er på (hvilken bokstav vi skal skrive)
+  if (wordCounter < words.length) {
+    wrongs.innerHTML = `Antall feil: ${wrongCounter}`
+    letter.innerHTML = `Bokstaven du skrev: ${key}`
+    const currentWord = words[wordCounter]
+    word.innerHTML = `<span style="color: green;">${currentWord.slice(0, positionCounter)}</span>${currentWord.slice(positionCounter)}`
+  } else {
+    word.innerHTML = "Ingen flere ord"
   }
 };
 
-// TODO: Lytt til keyup på window
-// TODO: Lytt til klikk på knappen
-button.addEventListener("", () => {
-  // TODO: Disable knappen etter at vi har trykket på den. Tips: Bruk .disabled = true
-  // TODO: Sett første ord
+
+window.addEventListener("keyup", handleKeyUp);
+
+button.addEventListener("click", () => {
+  button.disabled = true
+  setWord()
 });
