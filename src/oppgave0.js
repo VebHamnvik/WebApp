a2f6433398cec9b4a95976916be6530ac67d2e6c
Vebhamnvik <vebjornhamnvik95@gmail.com>
2024-09-03 12:52:51 +0200

    init intro til js

---
+++ src/oppgave0.js
@@ -0,0 +1,13 @@
+// Oppgave 1
+/*
+Lag en eventListener for button med id="remove-btn"
+Slett teksten med querySelector(#remove)
+
+// Oppgave 2
+// Oppgave 3
+// Oppgave 4
+const myList = ["item one", "item two", "item three"];
+// Oppgave 5
+// Oppgave 6
+// Oppgave 7
+// Oppgave 8
