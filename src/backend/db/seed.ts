bb925b2568699b4fb69832fb35eeaccd60d92994
VebHamnvik <vebjornhamnvik95@gmail.com>
2024-10-23 12:26:00 +0200

    db and tables

---
+++ src/backend/db/seed.ts
@@ -0,0 +1,33 @@
+import fs from "node:fs/promises";
+import { join } from "node:path";
+import type { DB } from "../db/db";
+import { ProjectType } from "../types/projectTypes";
+
+export const seed = async (db: DB) => {
+    const path = join(import.meta.dirname, "data.json");
+    const file = await fs.readFile(path, "utf-8");
+    const { projects } = JSON.parse(file) as {
+        projects: ProjectType[];
+    };
+
+    const insertProject = db.prepare(`
+        INSERT INTO projects (title, description, objective, language, created_at, status, is_public, tags, image) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`);
+
+        db.transaction(() => {
+            for (const project of projects) {
+                insertProject.run(
+                    project.title,
+                    project.description,
+                    project.objective,
+                    project.language,
+                    project.createdAt,
+                    project.status,
+                    project.isPublic ? 1 : 0,
+                    project.tags.join(','),
+                    project.image
+                );
+            };
+        })();
+
+};
--- src/backend/db/tables.ts
+++ src/backend/db/tables.ts
@@ -3,15 +3,14 @@ import type { DB } from "./db";
 export const createTables = (db: DB) => {
     db.exec(`
         CREATE TABLE IF NOT EXISTS projects (
-            id TEXT PRIMARY KEY,
-            title TEXT NOT NULL,
+            title TEXT PRIMARY KEY,
             description TEXT NOT NULL,
             objective TEXT NOT NULL,
             language TEXT NOT NULL,
             created_at TEXT NOT NULL,
             status TEXT NOT NULL,
             is_public BOOLEAN NOT NULL,
-            tags TEXT[] NOT NULL,
+            tags TEXT NOT NULL,
             image TEXT NOT NULL
         );
       `);
